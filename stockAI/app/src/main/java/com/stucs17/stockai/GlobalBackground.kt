40e725615d3a45c20d3dfce2f4d9f8d494dacf2f
shkim787 <shkim061198@gmail.com>
2022-04-20 21:36:56 +0900

    카카오 음성인식

--- stockAI/app/src/main/java/com/stucs17/stockai/GlobalBackground.kt
+++ stockAI/app/src/main/java/com/stucs17/stockai/GlobalBackground.kt
@@ -1,8 +1,6 @@
 package com.stucs17.stockai
 
-import android.app.Application;
 import android.content.ContentValues.TAG
-import android.content.Context
 import android.util.Log
 
 class GlobalBackground{
@@ -11,6 +9,7 @@ class GlobalBackground{
     fun logTest() {
         Log.d(TAG,"wow")
     }
+
 
 
 
